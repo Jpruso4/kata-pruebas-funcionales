024330db556410529a92ec8063eea579c63e5e88
Ralph Sliger <ralphsliger1@gmail.com>
2021-12-28 16:46:28 -0500

    Initial commit

---
+++ src/main/java/co/com/sofka/demo/Entities/Person.java
@@ -0,0 +1,29 @@
+package co.com.sofka.demo.Entities;
+
+public class Person
+{
+    private  String name;
+    private  String edad;
+    private  String telefono;
+
+    public Person() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEdad() {
+        return edad;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+
+}
