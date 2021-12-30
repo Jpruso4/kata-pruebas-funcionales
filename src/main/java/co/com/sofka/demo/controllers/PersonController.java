a184f617372f785a7f42585c2f7e3c46337eaa7b
Jpruso4 <74436239+Jpruso4@users.noreply.github.com>
2021-12-30 01:21:21 -0500

    Subiendo Kata Pruebas Funcionales

--- src/main/java/co/com/sofka/demo/controllers/PersonController.java
+++ src/main/java/co/com/sofka/demo/controllers/PersonController.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.util.List;
-
 @RestController
 @RequestMapping(value="/person")
 public class PersonController {
@@ -20,10 +18,12 @@ public class PersonController {
     public Mono<Void> post(@RequestBody Mono<Person> personMono){
         return personMono.flatMap(service::insert);
     }
+
     @GetMapping("/{id}")
     public Mono<Person> getPerson(@PathVariable("id") String id){
         return Mono.just(new Person());
     }
+
     @PutMapping
     public Mono<Void> update(@RequestBody Mono<Person> personMono){
         return Mono.empty();
@@ -33,6 +33,7 @@ public class PersonController {
     public Mono<Void> delete(@PathVariable("id") String id){
         return Mono.empty();
     }
+
     @GetMapping
     public Flux<Person> list(){
         return service.listAll();
